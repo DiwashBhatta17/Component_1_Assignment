d439e2a984158be885eb8f102cf780e1cfd365ed
diwashbhatta17 <bhattaaawash@gmail.com>
2023-10-29 21:34:53 +0545

    Input fields , buttons and Graphical Program Output setup a successfully

--- Component_1_Assignment/Form1.Designer.cs
+++ Component_1_Assignment/Form1.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.run = new System.Windows.Forms.Button();
+            this.syntax = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.BackColor = System.Drawing.Color.YellowGreen;
+            this.run.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.run.Location = new System.Drawing.Point(33, 542);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(135, 33);
+            this.run.TabIndex = 0;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = false;
+            // 
+            // syntax
+            // 
+            this.syntax.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.syntax.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.syntax.Location = new System.Drawing.Point(225, 542);
+            this.syntax.Name = "syntax";
+            this.syntax.Size = new System.Drawing.Size(135, 33);
+            this.syntax.TabIndex = 1;
+            this.syntax.Text = "Syntax";
+            this.syntax.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 440);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(654, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(554, 440);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 495);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(553, 22);
+            this.textBox2.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.ClientSize = new System.Drawing.Size(1262, 634);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.syntax);
+            this.Controls.Add(this.run);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button syntax;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Component_1_Assignment/Form1.cs
+++ Component_1_Assignment/Form1.cs
@@ -16,5 +16,10 @@ namespace Component_1_Assignment
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
